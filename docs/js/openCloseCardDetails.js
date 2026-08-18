7ad34a795d127bd38b244cd47d0ee84ebbfefba4
Adam Benjamin <adm.bjmn@gmail.com>
2026-08-18 15:31:13 +0100

    bug: workshop cards not opening

--- docs/js/openCloseCardDetails.js
+++ docs/js/openCloseCardDetails.js
@@ -4,14 +4,19 @@ function initialiseEventCards() {
     const openButton = card.querySelector(".open-event-details");
     const closeButton = card.querySelector(".close-event-details");
     const details = card.querySelector(".event-details");
-    openButton.addEventListener("click", () => {
-      details.classList.remove("translate-y-full");
-      details.classList.add("translate-y-0");
-    });
 
-    closeButton.addEventListener("click", () => {
-      details.classList.remove("translate-y-0");
-      details.classList.add("translate-y-full");
-    });
+    if (openButton) {
+      openButton.addEventListener("click", () => {
+        details.classList.remove("translate-y-full");
+        details.classList.add("translate-y-0");
+      });
+    }
+
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        details.classList.remove("translate-y-0");
+        details.classList.add("translate-y-full");
+      });
+    }
   });
 }
